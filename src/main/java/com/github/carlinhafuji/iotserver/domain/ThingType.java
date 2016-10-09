686a8729fc88eff74202499c8f051e42c1647477
Diego Allyson <diegoallyson@gmail.com>
2016-10-09 12:22:59 -0300

    Fix error

--- src/main/java/com/github/carlinhafuji/iotserver/domain/ThingType.java
+++ src/main/java/com/github/carlinhafuji/iotserver/domain/ThingType.java
@@ -1,9 +1,9 @@
 package com.github.carlinhafuji.iotserver.domain;
 
 public enum ThingType {
-    PLANTA(1),
-    BALANCA(2),
-    CARTEIRA_CHAVE_CARTEIRA(3);
+    PLANTA(0),
+    BALANCA(1),
+    CARTEIRA_CHAVE_CARTEIRA(2);
 
     private final int type;
     ThingType(int type) {
